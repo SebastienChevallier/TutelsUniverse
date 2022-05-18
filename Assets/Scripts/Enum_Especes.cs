96276e86cdceb2fb4ab0b4ed03ef25d6e2582809
AntoineJ00 <antoinejunique13@gmail.com>
2022-05-18 15:37:14 +0200

    Spawn des espèces, changements via UIs ;)

---
+++ Assets/Scripts/Enum_Especes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enum_Especes : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public enum Especes
+    {
+        Potamouss,
+        Lapillon,
+        Lomphore,
+        Raynodon,
+        Cerfeuille,
+    }
+
+}
