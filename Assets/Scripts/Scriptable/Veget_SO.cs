765708e82a3b27ad2029b940ead82ff9c9e9a5d8
AntoineJ00 <antoinejunique13@gmail.com>
2022-05-30 19:16:46 +0200

    Ajout pour tester des différentes textures de Laura Test d'évolution des pousses d'arbre

---
+++ Assets/Scripts/Scriptable/Veget_SO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Veget_SO", menuName = "ScriptableObjects/Veget_SO", order = 1)]
+public class Veget_SO : ScriptableObject
+{
+    public string Name;
+    public GameObject Mesh_1;
+    public GameObject Mesh_2;
+    public GameObject Mesh_3;
+    public Material Material_1;
+    public Material Material_2;
+    public Material Material_3;
+    public int Longévité;
+    public AnimationCurve _CourbeVitalite;
+    public AnimationCurve _CourbeScale;
+}
