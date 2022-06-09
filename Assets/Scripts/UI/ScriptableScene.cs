600c76c9058857b48154a0f5782f64b73c45f677
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2022-06-09 10:45:38 +0200

    Scene

---
+++ Assets/Scripts/UI/ScriptableScene.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Scene", menuName = "ScriptableObjects/Scene", order = 1)]
+public class ScriptableScene : ScriptableObject
+{
+    public int _SceneIndex;
+}
--- Assets/Scripts/UI/UI_Menu.cs
+++ Assets/Scripts/UI/UI_Menu.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 
 public class UI_Menu : MonoBehaviour
 {
+    public ScriptableScene scene;
+    public void Begin()
+    {
+        scene._SceneIndex = 6;
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync(1);
+    }
+
     public void Play()
     {
+        scene._SceneIndex = 2;
         SceneManager.LoadScene(2, LoadSceneMode.Additive);
         SceneManager.UnloadSceneAsync(1);
     }
 
     public void SceneAssets()
     {
-        SceneManager.LoadScene(5, LoadSceneMode.Additive);
+        scene._SceneIndex = 5;
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
         SceneManager.UnloadSceneAsync(1);
     }
 
